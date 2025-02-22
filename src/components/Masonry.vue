f776742ae7d242159f319045c942fdfe17d1b6d4
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-02-22 22:14:31 +0800

    feat: add mansonry layout

---
+++ src/components/Masonry.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hfall">
+    <NuxtImg v-for="img in images" :src="img" :alt="img" class="item hover:shadow-xl transition-shadow"></NuxtImg>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  images: {
+    type: Array<string>
+  }
+})
+
+</script>
+
+<style scoped>
+.hfall {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+  padding: 1rem 2rem;
+  /* width: 500px; */
+}
+
+.item {
+  flex-grow: 1;
+  object-fit: cover;
+  height: 300px;
+  max-width: 100%;
+  border-radius: 12px;
+  border: 1px rgb(var(--color-gray-500));
+}
+</style>
