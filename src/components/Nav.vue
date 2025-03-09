c6708092f3bdb921b5e5cfe48a695fde16b5f8fc
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-03-09 16:27:16 +0800

    refactor: better Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-48 min-h-full px-2 border-r-2 border-gray-200">
-    <h2 class="px-4 pt-4 pb-2.5">Gallery</h2>
+  <div class="w-[15vw] min-h-full py-2 pl-4 pr-2 border-r-2 border-gray-200 max-sm:hidden">
+    <h2 class="px-6 pt-4 pb-2.5 text-2xl">Gallery</h2>
     <UNavigationMenu :items="links" orientation="vertical"
-      :ui="{ root: 'size-full', item: 'text-xl', link: 'py-2.5 px-4', linkLeadingIcon: 'text-xl' }">
+      :ui="{ root: 'size-full', link: 'py-2.5 px-[8%] text-[1.1rem]', linkLeadingIcon: 'text-xl mr-1', separator: 'my-2' }">
     </UNavigationMenu>
   </div>
 </template>
@@ -14,7 +14,7 @@ const links = ref([
     {
       label: "Photo",
       icon: "i-lucide-image",
-      to: "/photo"
+      to: "/"
     },
     {
       label: "Album",
@@ -23,6 +23,7 @@ const links = ref([
     {
       label: "Timeline",
       icon: "i-material-symbols-timer-play-outline",
+      to: "/timeline",
       disabled: true
     }
   ],
