9ef5d17cc76c7057cad6b4e3bbcce5ad16ef174c
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-04-14 00:42:19 +0800

    fix: now Nav is fixed

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-[100vh]">
-    <div class="">
+    <div class="fixed z-20">
       <Nav />
     </div>
+    <div class="sm:w-[15vw] flex-none"></div>
     <div class="grow p-2 sm:p-0">
       <slot />
     </div>
