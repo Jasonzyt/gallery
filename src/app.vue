4e79d2e4a5d2d805659c2e14b272f4f080de92b9
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-06-16 00:52:31 +0800

    feat: add website title

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,15 @@
 <template>
+
+  <Head>
+    <title>Jasonzyt's Gallery</title>
+    <meta name="description" content="Jasonzyt's Gallery" />
+    <meta name="keywords" content="jasonzyt,gallery,photo,photography" />
+  </Head>
+
   <UApp>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </UApp>
+
 </template>
