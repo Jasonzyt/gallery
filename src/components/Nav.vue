c8179166e432389536534649038f2871a6a0db0e
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-06-16 01:29:51 +0800

    fix: fix dark mode

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,7 +3,7 @@
   <div class="relative h-full">
     <!-- Mobile menu toggle button - only visible on small screens -->
     <button @click="toggleMenu" v-show="!isMenuOpen"
-      class="fixed top-4 left-4 z-30 bg-white rounded-md p-2 shadow-md hidden max-sm:flex items-center justify-center"
+      class="fixed top-4 left-4 z-30 bg-white dark:bg-[#0f172b] rounded-md p-2 shadow-md hidden max-sm:flex items-center justify-center"
       aria-label="Toggle navigation menu">
       <Icon name="i-lucide-menu" size="1.4em" />
     </button>
@@ -17,7 +17,7 @@
 
     <!-- Navigation drawer - responsive for both desktop and mobile -->
     <div
-      class="w-full min-h-full pt-2 pb-6 pl-2 pr-2 sm:pl-4 sm:border-r-2 border-gray-200 flex flex-col bg-white max-sm:fixed max-sm:left-0 max-sm:top-0 max-sm:bottom-0 max-sm:w-[80%] max-sm:z-20 max-sm:transform max-sm:transition-transform max-sm:duration-300 max-sm:ease-in-out"
+      class="w-full min-h-full pt-2 pb-6 pl-2 pr-2 sm:pl-4 sm:border-r-2 border-gray-200 dark:border-gray-800 max-sm:bg-white max-sm:dark:bg-[#0f172b] flex flex-col max-sm:fixed max-sm:left-0 max-sm:top-0 max-sm:bottom-0 max-sm:w-[80%] max-sm:z-20 max-sm:transform max-sm:transition-transform max-sm:duration-300 max-sm:ease-in-out"
       :class="isMenuOpen ? 'max-sm:translate-x-0' : 'max-sm:-translate-x-full'">
       <h2 class="px-4 pt-4 pb-2.5 text-2xl">
         <Icon name="my:gallery" size="1.4em" class="align-text-bottom mr-2" />Gallery
